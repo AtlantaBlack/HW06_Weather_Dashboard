374993bd24096b8d826460f6fcf928242c1dd80a
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-03-27 23:23:36 +1100

    updated weather API to use one-call API function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var weatherAPIkey = "70ae3d7cda7e676a90d911c1ff2798ed";
 
 var cityNameDisplayArea = document.querySelector("#city-name-display");
+var todaysDateArea = document.querySelector("#todays-date-display");
 var weatherDisplayArea = document.querySelector("#weather-display");
 
 var submitButton = document.querySelector("#get-weather-button");
@@ -13,7 +14,7 @@ function submitUserInput(event) {
 
     var city = document.querySelector("#city-name");
     city = city.value.trim();
-    console.log(city);
+    // console.log(city);
 
     if (!city) {
         alert("Please write a valid city name.");
@@ -28,7 +29,7 @@ function submitUserInput(event) {
 function geocodeTheCity(location) {
     var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=" + weatherAPIkey;
 
-    console.log(geocodeUrl);
+    // console.log(geocodeUrl);
 
     fetch(geocodeUrl)
         .then(function (response) {
@@ -39,7 +40,7 @@ function geocodeTheCity(location) {
             }
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
 
             if (data.length === 0) {
                 alert("Sorry, we couldn't find that city!\nPlease try again.");
@@ -50,7 +51,7 @@ function geocodeTheCity(location) {
                 var cityName = data[0].name;
                 var countryCode = data[0].country;
                 displayCityName(cityName, countryCode);
-                getWeather(lat, lon);
+                getWeatherDetails(lat, lon);
             }
         })
         .catch(function (error) {
@@ -60,7 +61,7 @@ function geocodeTheCity(location) {
 
 // display the city name and country
 function displayCityName(localName, countryCode) {
-    console.log("city: " + localName + ", country: " + countryCode);
+    // console.log("city: " + localName + ", country: " + countryCode);
 
     cityNameDisplayArea.textContent = "";
 
@@ -71,12 +72,12 @@ function displayCityName(localName, countryCode) {
 }
 
 // grab weather data from the city at specified latitude/longitude
-function getWeather(lat, lon) {
-    console.log("lat: " + lat + " lon: " + lon);
+function getWeatherDetails(lat, lon) {
+    // console.log("lat: " + lat + " lon: " + lon);
 
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + weatherAPIkey + "&units=metric";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&appid=" + weatherAPIkey + "&units=metric";
 
-    console.log(queryUrl);
+    // console.log(queryUrl);
 
     fetch(queryUrl)
         .then(function (response) {
@@ -87,26 +88,33 @@ function getWeather(lat, lon) {
             }
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
 
             // extract icon data and make a url for it
-            var iconCode = data.weather[0].icon;
-            var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+            var iconCode = data.current.weather[0].icon;
+            var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+
+            // console.log(iconUrl);
 
             // extract weather description data and run capitalise the first letter of first word
-            var description = data.weather[0].description;
+            var description = data.current.weather[0].description;
                 function capitaliseFirstLetter(str) {
                     let capitalised = str.charAt(0).toUpperCase() + str.slice(1);
                     return capitalised;
                 }
 
             var weatherDesc = capitaliseFirstLetter(description);
-            var temperature = data.main.temp;
-            var feelsLike = data.main.feels_like;
-            var humidity = data.main.humidity;
-            var windSpeed = data.wind.speed;
+            var temperature = data.current.temp;
+            var uvIndex = data.current.uvi;
+            var humidity = data.current.humidity;
+            var windSpeed = data.current.wind_speed;
 
-            displayTheWeather(iconUrl, weatherDesc, temperature, feelsLike, humidity, windSpeed);
+            var currentTimestamp = data.current.dt;
+            var daily = data.daily;
+
+            convertTimestamp(currentTimestamp);
+            displayTheWeather(iconUrl, weatherDesc, temperature, uvIndex, humidity, windSpeed);
+            displayForecast(daily);
             
         })
         .catch(function (error) {
@@ -120,8 +128,37 @@ function round(value, precision) {
     return Math.round(value * multiplier) / multiplier;
 }
 
+// convert the unix timestamp into a readable date
+function convertTimestamp(stamp) {
+    // console.log("stamp " + stamp);
+    var currentDay = new Date(stamp * 1000);
+
+    // console.log("currentday " + currentDay);
+
+    var year = currentDay.getFullYear();
+    var month = ("0" + (currentDay.getMonth() + 1)).slice(-2);
+    var day = ("0" + currentDay.getDate()).slice(-2);
+
+    // console.log("day " + day);
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var dayOfTheWeek = days[currentDay.getDay()];
+
+    date = dayOfTheWeek + " " + day + "-" + month + "-" + year;
+
+    displayTodaysDate(date);
+}
+
+function displayTodaysDate(date) {
+    todaysDateArea.textContent = "";
+
+    var titleDate = document.createElement("h5");
+    titleDate.textContent = date;
+    todaysDateArea.appendChild(titleDate);
+}
+
 // display the weather results
-function displayTheWeather(icon, desc, temp, feel, humidity, wind) {
+function displayTheWeather(icon, desc, temp, uv, humidity, wind) {
     weatherDisplayArea.textContent = "";
 
     // create first row for icon and weather description display
@@ -130,18 +167,14 @@ function displayTheWeather(icon, desc, temp, feel, humidity, wind) {
 
     // create element for icon and append to display
     var columnOne = document.createElement("div");
-    columnOne.classList = "col-6";
+    columnOne.classList = "col-12";
     // make weather icon as image
     var weatherIcon = document.createElement("img");
     weatherIcon.setAttribute("src", icon);
     columnOne.append(weatherIcon);
-    rowOne.appendChild(columnOne);
+    columnOne.append(" " + desc);
 
-    // create element for weather description and append to display
-    var columnTwo = document.createElement("div");
-    columnTwo.classList = "col-6";
-    columnTwo.textContent = desc;
-    rowOne.appendChild(columnTwo);
+    rowOne.appendChild(columnOne);
 
     weatherDisplayArea.appendChild(rowOne);
 
@@ -152,7 +185,7 @@ function displayTheWeather(icon, desc, temp, feel, humidity, wind) {
     // create element for current temp and append to display
     var columnThree = document.createElement("div");
     columnThree.classList = "col-6";
-    columnThree.textContent = "Current temperature: " + round(temp, 1) + " °C";
+    columnThree.textContent = "Current temp: " + round(temp, 1) + " °C";
     rowTwo.appendChild(columnThree);
 
     // create element for humidity and append to display
@@ -163,14 +196,14 @@ function displayTheWeather(icon, desc, temp, feel, humidity, wind) {
 
     weatherDisplayArea.appendChild(rowTwo);
 
-    // create third row for 'temp it feels like' and wind speed
+    // create third row for uv index and wind speed
     var rowThree = document.createElement("div");
     rowThree.classList = "row";
 
-    // create element for 'currently feels like' and append to display
+    // create element for uv index and append to display
     var columnFive = document.createElement("div");
     columnFive.classList = "col-6";
-    columnFive.textContent = "Feels like: " + round(temp, 1) + " °C";
+    columnFive.innerHTML = "UV Index: " + "<span id='uvi-span'>" + colourChange(uv) + "</span>";
     rowThree.appendChild(columnFive);
 
     // create element for wind speed and append to display
@@ -180,7 +213,22 @@ function displayTheWeather(icon, desc, temp, feel, humidity, wind) {
     rowThree.appendChild(columnSix);
 
     weatherDisplayArea.appendChild(rowThree);
+}
 
+function colourChange(uv) {
+    var uvispan = document.getElementById("uvi-span");
+    if (uv > 1) {
+        uvispan.style.backgroundColor = "red";
+        console.log(uv);
+    } else {
+        console.log(uv);
+    }
+
+}
+
+// display forecast for next five days
+function displayForecast(daily) {
+    console.log(daily)
 }
 
 submitButton.addEventListener("click", submitUserInput);
